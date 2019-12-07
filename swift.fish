ec0018ed3ef6041ac669732f3539c9d28b5b013d
archmagees <archmagees.dev@gmail.com>
2019-12-07 23:25:56 +0800

    ✨ Support options with argument

--- swift.fish
+++
@@ -1,9 +0,0 @@
-# author: Arthur Liu
-# email:  archmagees.dev@gmail.com
-# github: archmagees
-
-################################################################################
-# default options
-complete -c swift -f -a '' -r
-
-complete -c swift -a "package" -f
